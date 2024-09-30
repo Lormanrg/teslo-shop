f3a5a9bedcb161e8bfa130ea4c4eeef019439821
Lorman <lormanrg@gmail.com>
2024-09-29 20:20:41 -0400

    Starting teslo-shop project

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USERNAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    ProductsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
