9c46b1dacc23095b4cc4a7b2ffca3e76e0007fa7
Lorman <lormanrg@gmail.com>
2024-10-07 21:27:54 -0400

    CRUD completed

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -146,4 +146,14 @@ export class ProductsService {
 
     throw new InternalServerErrorException('Unexpected error, check logs');
   }
+
+  async deleteAllProducts() {
+    const query = this.productRepository.createQueryBuilder('product');
+
+    try {
+      return await query.delete().where({}).execute();
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
+  }
 }
