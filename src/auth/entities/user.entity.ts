e45c4492126b3673725c24e7bfafe85c9207b692
Lorman <lormanrg2091@gmail.com>
2025-01-09 14:58:43 -0400

    develop in process

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,35 @@
+/* eslint-disable prettier/prettier */
+
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text', {
+        unique: true,
+        nullable: false
+    })
+    email: string;
+
+    @Column('text', {
+        nullable: false
+    })
+    password: string;
+
+    @Column('text')
+    fullName: string
+
+     @Column('boolean', {
+         default: true
+     })
+    isActive: boolean;
+
+    @Column('text', {
+        array: true,
+        default: ['user'],
+
+    })
+    roles: string[];
+}
